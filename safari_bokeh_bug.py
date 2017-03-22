dfc7a722752ba13a3c9e3241f53ae26aaef80e86
Angelo Fausti <afausti@lsst.org>
2017-03-22 01:09:56 -0700

    Added minimum code example to reproduce the bug

---
+++ safari_bokeh_bug.py
@@ -0,0 +1,23 @@
+""" This example demonstrates a bug found on Safari>=10.1
+dropping the web socket connection to the bokeh server.
+"""
+
+from bokeh.io import curdoc
+from bokeh.models.widgets import Select, Div
+from bokeh.layouts import column
+
+# configure some bokeh widgets
+select = Select(title="Options:", value="0", options=["0","1","2","3"])
+label = Div(text="Selected option: 0")
+
+# create a layout for this document
+layout = column(select, label)
+curdoc().add_root(layout)
+
+# update the text label to the selected option
+def update(attr, old, new):
+    label.text = "Selected option: {}".format(new)
+
+select.on_change("value", update)
+
+
